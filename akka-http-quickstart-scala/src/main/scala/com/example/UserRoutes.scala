625ab7a67fc522d1161f4da2ba923fcd166e2b20
roisin-jin <jin@ringkinces.com>
2020-01-26 01:09:08 +0000

    add authenticator for routes path

--- akka-http-quickstart-scala/src/main/scala/com/example/UserRoutes.scala
+++ akka-http-quickstart-scala/src/main/scala/com/example/UserRoutes.scala
@@ -4,11 +4,12 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
 
-import scala.concurrent.Future
+import scala.concurrent.{ ExecutionContext, Future }
 import com.example.UserRegistry._
 import akka.actor.typed.ActorRef
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.AskPattern._
+import akka.http.scaladsl.server.directives.Credentials
 import akka.util.Timeout
 
 //#import-json-formats
@@ -19,9 +20,20 @@ class UserRoutes(userRegistry: ActorRef[UserRegistry.Command])(implicit val syst
   import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
   import JsonFormats._
   //#import-json-formats
-
+  implicit val executionContext: ExecutionContext = system.executionContext
   // If ask takes more time than this to complete the request is failed
   private implicit val timeout = Timeout.create(system.settings.config.getDuration("my-app.routes.ask-timeout"))
+
+  def requestUserAuthenticator(credentials: Credentials): Future[Option[String]] =
+    credentials match {
+      case p:Credentials.Provided =>
+        Future {
+          // potentially
+          if (p.verify("p4ssw0rd")) Some(p.identifier)
+          else None
+        }
+      case _ => Future.successful(None)
+    }
 
   def getUsers(): Future[Users] =
     userRegistry.ask(GetUsers)
@@ -35,45 +47,43 @@ class UserRoutes(userRegistry: ActorRef[UserRegistry.Command])(implicit val syst
   //#all-routes
   //#users-get-post
   //#users-get-delete
-  val userRoutes: Route =
-    pathPrefix("users") {
+  val userRoutes: Route = Route.seal(
+    pathPrefix("users")(authenticateBasicAsync("realm", requestUserAuthenticator)( _ =>
       concat(
-        //#users-get-delete
-        pathEnd {
-          concat(
-            get {
-              complete(getUsers())
-            },
-            post {
-              entity(as[User]) { user =>
-                onSuccess(createUser(user)) { performed =>
-                  complete((StatusCodes.Created, performed))
+          //#users-get-delete
+          pathEnd {
+            concat(
+              get(complete(getUsers())),
+              post {
+                entity(as[User]) { user =>
+                  onSuccess(createUser(user)) { performed =>
+                    complete((StatusCodes.Created, performed))
+                  }
                 }
-              }
-            })
-        },
-        //#users-get-delete
-        //#users-get-post
-        path(Segment) { name =>
-          concat(
-            get {
-              //#retrieve-user-info
-              rejectEmptyResponse {
-                onSuccess(getUser(name)) { response =>
-                  complete(response.maybeUser)
+              })
+          },
+          //#users-get-delete
+          //#users-get-post
+          path(Segment) { name =>
+            concat(
+              get {
+                //#retrieve-user-info
+                rejectEmptyResponse {
+                  onSuccess(getUser(name)) { response =>
+                    complete(response.maybeUser)
+                  }
                 }
-              }
-              //#retrieve-user-info
-            },
-            delete {
-              //#users-delete-logic
-              onSuccess(deleteUser(name)) { performed =>
-                complete((StatusCodes.OK, performed))
-              }
-              //#users-delete-logic
-            })
-        })
+                //#retrieve-user-info
+              },
+              delete {
+                //#users-delete-logic
+                onSuccess(deleteUser(name)) { performed =>
+                  complete((StatusCodes.OK, performed))
+                }
+                //#users-delete-logic
+              })
+          }))
       //#users-get-delete
-    }
+    ))
   //#all-routes
 }
