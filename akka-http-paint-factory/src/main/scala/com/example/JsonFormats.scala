30268633b4653f09dd043e4be0acd3b85f37ee73
roisin-jin <jin@ringkinces.com>
2020-01-28 01:08:25 +0000

    rename app

---
+++ akka-http-paint-factory/src/main/scala/com/example/JsonFormats.scala
@@ -0,0 +1,19 @@
+package com.example
+
+import com.example.UserRegistry.ActionPerformed
+import spray.json.DefaultJsonProtocol
+
+object JsonFormats extends DefaultJsonProtocol  {
+  // import the default encoders for primitive types (Int, String, Lists etc)
+
+  implicit val userJsonFormat = jsonFormat3(User)
+  implicit val usersJsonFormat = jsonFormat1(Users)
+
+  implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed)
+
+  implicit val paintDemandJsonFormat = jsonFormat2(PaintDemand)
+  implicit val paintDemandsJsonFormat = jsonFormat2(PaintDemands)
+  implicit val paintRequestJsonFormat = jsonFormat2(PaintRequest)
+  implicit val internalRequestJsonFormat = jsonFormat3(InternalRequest)
+}
+
